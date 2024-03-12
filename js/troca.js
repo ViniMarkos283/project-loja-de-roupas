304b9d9a825839abf478f79dec2a8e5b9cf5ada6
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-03-12 12:10:34 -0300

    Update troca.js

--- js/troca.js
+++ js/troca.js
@@ -1,10 +1,10 @@
-function setItem(num){
-    const itens = Products()
-
+function setClothes(num){
+    sessionStorage.setItem('item', num)
 }
 
 function loadOneProduct() {
-    sessionStorage.getItem('item');
+    const itens = products()
+    const n = sessionStorage.getItem('item')
 
     image = document.querySelector('#image')
     nm = document.querySelector('#name')
@@ -18,9 +18,9 @@ function loadOneProduct() {
     console.log(size)
     console.log(category)
 
-    image.src = itens.img
-    nm.value = itens.desc
-    description.value = "Sobre o produto: \n" + itens.desc
-    size.value = "Tamanho: " + itens.size
-    category.value = "Categoria: " + itens.category
+    image.src = itens[n].img
+    nm.value = itens[n].desc
+    description.value = "Sobre o produto: \n" + itens[n].desc
+    size.value = "Tamanho: " + itens[n].size
+    category.value = "Categoria: " + itens[n].category
 }
