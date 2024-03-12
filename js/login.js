6aced27cce2e5bfd8fbb20d1d22be033081bf2d2
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-03-12 20:09:41 -0300

    ajuste na cadastro.js e login.js

---
+++ js/login.js
@@ -0,0 +1,23 @@
+
+function login() {
+    var username = document.querySelector('#login').value;
+    var senha = document.querySelector('#senha').value;
+    var bd_username = sessionStorage.username
+    var bd_senha = sessionStorage.senha
+    if (login == null || senha == null) {
+
+    }
+    if (bd_username == username && bd_senha == senha) {
+        sessionStorage.username = username
+        window.location.href = "../html/index.html"
+    }
+    else {
+        console.log(username)
+        console.log(senha)
+        console.log(bd_username)
+        console.log(bd_senha)
+        let info = document.getElementById("info")
+        info.innerHTML += `<h2>usuario ou senha incorretos</h2>                      
+`
+    }
+}
