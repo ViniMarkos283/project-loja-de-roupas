d8f8ddf8fccd2ecfbdfc225c67520bf5bf22730a
Aluno <pedro.bernardo4561@gmail.com>
2024-06-10 11:04:50 -0300

    ajuste no perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,31 +1,46 @@
-// Função para renderizar os itens do carrinho na página
 document.addEventListener('DOMContentLoaded', async () => {
 
-    const user = localStorage.getItem("userPass")
+    const session = sessionStorage.getItem("userPass");
+    console.log("Session:", session);
 
-        // cartItems.forEach(item => {
-             const profileElement = document.createElement('div');
-        //     // productElement.classList.add('product');
+    const users = JSON.parse(session); // Alteração: agora é um array de usuários
+    console.log("Users:", users);
 
+    const profileElement = document.querySelector(".infos");
+
+    if (users && users.length > 0) { // Verifica se há usuários no array
+        const user = users[0]; // Obtém o primeiro usuário do array
         profileElement.innerHTML = `
-            <div class="row">
-                <div class="col">
-                        <p>nome: ${user.nome}</p>
-                        <p>data de nascimento: ${user.dtNasc}</p>
-                        <p>Email: ${user.Email}</p>v
-                        <p>Endereço: ${user.Endereco}</p>
-                        <p>cidade: ${user.cidade}</p>
-                        <p>estado: ${user.estado}</p>
-                        <p>telefone fixo: ${user.telFixo}</p>
-                        <p>telefone celular: ${user.telCelular}</p>                     
-                </div>
-            </div>              
-            `;
-        // });
+        <div class="col-md-6">
+        <p><strong>Nome:</strong> ${user.full_name}</p>
+        <p><strong>Data de nascimento:</strong> ${user.birth_date}</p>
+        <p><strong>Email:</strong> ${user.email}</p>
+    </div>
+    <div class="col-md-6">
+        <p><strong>Estado:</strong> ${user.state}</p>
+        <p><strong>Telefone fixo:</strong> ${user.phone_home}</p>
+        <p><strong>Telefone celular:</strong> ${user.phone_mobile}</p>
+    </div>
+    <div class="col-md-6">
+    <p><strong>Endereço:</strong> ${user.address}</p>
+        <p><strong>Cidade:</strong> ${user.city}</p>
+        </div>
+
+        `;
+    } else {
+        profileElement.innerHTML = `<p>Nenhum usuário encontrado.</p>`;
+    }
 });
 
 
+
 // async function removeItem(cartId, idProd) {
+// console.log(profileElement);
+// console.log(user.city);
+// console.log(user.full_name);
+// console.log(user);
+// console.log(session.full_name);
+// console.log(session);
 //     try {
 //         await fetchDeleteItem(cartId, idProd);
 //         // document.location.reload();
